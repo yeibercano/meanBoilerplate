4abf8a1348a75de69ff3c2b7bab1a540132b6a4a
Yeiber Cano <yeibercano@gmail.com>
2016-04-21 14:28:48 -0700

    connects the por to the server and exports the server

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ var express = require('express'),
 var app = express(); //creates an express server call app
 // port for server
 var port = process.env.PORT || 4449;
+
+//connects the port to the app server
+app.listen(port);
+//prints the server running to the console when the server starts
+console.log('Server now listening on port ' + port);
+//exports the server
+module.exports = app;
