47a787b9f302e63987851b9987c7422ea9d17244
Yeiber Cano <yeibercano@gmail.com>
2016-04-21 14:23:39 -0700

    creates a server app

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ var express = require('express'),
     bodyParser = require('body-parser'), //pulls information from the post request
     morgan = require('morgan'); //logs requests to the console
     
+// creates the server
+var app = express(); //creates an express server call app
