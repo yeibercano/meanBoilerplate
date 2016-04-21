c1a88da6869fd58e485a0491ed2e10c4822a13f3
Yeiber Cano <yeibercano@gmail.com>
2016-04-21 14:25:23 -0700

    assigns a 4449 port to the server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express'),
     mongoose = rquire('mongoose'), // ODM to manipulate data in mongodb
     bodyParser = require('body-parser'), //pulls information from the post request
     morgan = require('morgan'); //logs requests to the console
-    
+
 // creates the server
 var app = express(); //creates an express server call app
+// port for server
+var port = process.env.PORT || 4449;
