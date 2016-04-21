9df3779d67746b4e954e5802f2341e6d1746e843
Yeiber Cano <yeibercano@gmail.com>
2016-04-21 14:22:52 -0700

    creates a package.json and requires module for sever

---
+++ server.js
@@ -0,0 +1,6 @@
+//require modules
+var express = require('express'), 
+    mongoose = rquire('mongoose'), // ODM to manipulate data in mongodb
+    bodyParser = require('body-parser'), //pulls information from the post request
+    morgan = require('morgan'); //logs requests to the console
+    
