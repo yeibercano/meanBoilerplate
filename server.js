306bfe2810ef1c968f4c752bf25c21e1fd5ba267
Yeiber Cano <yeibercano@gmail.com>
2016-04-21 16:05:27 -0700

    adds a default route

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 //require modules
-var express = require('express'), 
+var express = require('express'),
     mongoose = require('mongoose'), // ODM to manipulate data in mongodb
     bodyParser = require('body-parser'), //pulls information from the post request
     morgan = require('morgan'); //logs requests to the console
 
 // creates the server
 var app = express(); //creates an express server call app
+
 // port for server
 var port = process.env.PORT || 4451;
 
+
+//routes 
+
+//default route
+app.get('/', function(req, res){
+    res.send('we are connecting to a get resquest')
+});
+
+
 //connects the port to the app server
 app.listen(port);
+
 //prints the server running to the console when the server starts
 console.log('Server now listening on port ' + port);
+
 //exports the server
 module.exports = app;
